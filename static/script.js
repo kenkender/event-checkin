af9007c74f4e0be29f1ca17ce3c94d8952b61eda
kenkender <emptyken37@gmail.com>
2025-08-23 19:47:05 +0700

    ขยายตัวหนังสือกรอบเช็คอินแล้ว 6

--- static/script.js
+++ static/script.js
@@ -157,8 +157,8 @@ async function checkIn(){
     // ถ้าเช็คอินซ้ำ => เปิด modal แจ้งเตือน
     if (data.already) {
       openDupModal(`
-        <div style="margin-bottom:8px;">คุณได้เช็คอินเรียบร้อยแล้ว</div>
-        <div style="font-weight:700; margin-bottom:6px;">
+        <div style="margin-bottom:8px; font-size:1.6em;">คุณได้เช็คอินเรียบร้อยแล้ว</div>
+        <div style="font-weight:700; margin-bottom:6px; font-size:1.6em;">
           ที่นั่งของคุณคือ <span style="color:#e32c2c">${data.seat}</span>
         </div>
         <div style="opacity:.9; font-size:1.6em;">You have already checked in.<br>Your seat is <b>${data.seat_en || data.seat}</b>.</div>
